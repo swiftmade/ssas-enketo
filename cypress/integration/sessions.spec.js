224774b36a9386d175c8e5998ecb78db32986a0b
Ahmet Özışık <ozisikahmet@gmail.com>
2018-09-11 18:41:34 +0300

    add more tests for sessions

--- cypress/integration/sessions.spec.js
+++ cypress/integration/sessions.spec.js
@@ -1,25 +1,57 @@
-context('Survey session', () => {
+describe('Survey session', () => {
 
-
-  beforeEach(function() {
+  before(function() {
     indexedDB.deleteDatabase('_pouch_sessions')
   })
 
   it('is not activated in online mode', () => {
     cy.visit('/survey.html?survey=test.json&mode=online')
-      .wait(500)
-      .get('.modal-container').should('not.visible')
+      .wait(200)
+      .get('#surveyModal').should('not.exist')
       .get('.save-progress').should('not.visible');
   })
 
   it('can be created in offline mode', () => {
     cy.visit('/survey.html?survey=test.json&mode=offline')
-      .wait(500)
-      .get('.modal-container').should('visible')
+      .wait(200)
+      .get('#surveyModal').should('be.visible')
+      // Type in the session name and create it
       .get('.modal-body input').type('Test session')
       .get('.modal-body button').contains('Start').click()
-      .wait(500)
+      .wait(200)
+      // Make sure the save button is visible
+      .get('#surveyModal').should('not.be.visible')
       .get('.save-progress').should('visible');
+  })
+
+  it('can be deleted by user', () => {
+
+    cy.on('window:confirm', () => {
+      return true
+    })
+    // Session is persisted. Try deleting it.
+    cy.visit('/survey.html?survey=test.json&mode=offline')
+      .wait(200)
+      // Session modal
+      .get('#surveyModal').should('be.visible')
+      .get('.session-name').contains('Test session').should('be.visible')
+      .get('.action--delete_session').click()
+      .get('.session-name').should('not.exist')
+  })
+
+  it('can be saved by clicking the save icon', () => {
+    // On next refresh, session should not be visible.
+    cy.reload()
+      .wait(200)
+      // Session modal
+      .get('#surveyModal').should('be.visible')
+      .get('.session-name').should('not.exist')
+      .get('.modal-body input').type('Test session')
+      .get('.modal-body button').contains('Start').click()
+      .wait(200)
+      .get('#surveyModal').should('not.be.visible')
+      .get('.save-progress').click()
+      .get('.toast.toast-success').should('be.visible')
   })
 
 
