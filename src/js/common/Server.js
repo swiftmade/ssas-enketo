1c478209f9656ca1268c1fee04f376a8374f8bb6
Ahmet Özışık <ozisikahmet@gmail.com>
2019-03-27 20:53:41 +0300

    refactor Server.js, move some logic about enketo form submissions here

--- src/js/common/Server.js
+++ src/js/common/Server.js
@@ -42,11 +42,51 @@ export default class Server {
         return data
     }
 
-    async postForm(url, form, headers = {}) {
-        const {data} = await axios.post(url, form, {
-            headers
-        })
+    /**
+     * @param Session session 
+     */
+    async postForm(session, onProgress = null) {
+        
+        const form = new FormData()
+        form.append('Date', new Date().toUTCString())
+        form.append('xml_submission_file', new Blob([session.data.xml]))
+
+        if (typeof session.data.payload === 'object') {
+            form.append(
+                'Payload',
+                JSON.stringify(session.data.payload)
+            )
+        }
+
+        const submitUrl = queryParams.getPath('submit')
+        const config = {
+            headers: this._headersForSession(session),
+        }
+
+        if (onProgress) {
+            config.onUploadProgress = progressEvent => onProgress(
+                progressEvent.total > 0
+                    ? progressEvent.loaded / progressEvent.total
+                    : 0
+            )
+        }
+
+        const {data} = await axios.post(
+            submitUrl,
+            form,
+            config,
+        )
+
         return data
     }
 
+    _headersForSession(session) {
+        return {
+            'Content-Type': 'multipart/form-data',
+            // Open-rosa Headers
+            'X-OpenRosa-Version': '1.0',
+            'X-OpenRosa-Instance-Id': session.data.instance_id,
+            'X-OpenRosa-Deprecated-Id': session.data.deprecated_id,
+        }
+    }
 }
--- src/js/submission/drivers/Http.js
+++ src/js/submission/drivers/Http.js
@@ -8,31 +8,6 @@ export default class Http {
     }
 
     async submit() {
-        const submitUrl = queryParams.getPath('submit')
-
-        const form = new FormData()
-        form.append('Date', new Date().toUTCString())
-        form.append('xml_submission_file', new Blob([this.session.data.xml]))
-
-        const headers = {
-            'Content-Type': 'multipart/form-data',
-            // Open-rosa Headers
-            'X-OpenRosa-Version': '1.0',
-            'X-OpenRosa-Instance-Id': this.session.data.instance_id,
-            'X-OpenRosa-Deprecated-Id': this.session.data.deprecated_id,            
-        }
-
-        if (typeof this.session.data.payload === 'object') {
-            form.append(
-                'Payload',
-                JSON.stringify(this.session.data.payload)
-            )
-        }
-
-        return await Server.create().postForm(
-            submitUrl,
-            form,
-            headers
-        )
+        return await Server.create().postForm(this.session)
     }
 }
