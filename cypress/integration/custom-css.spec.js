0f7bbcc7399ae554ed6e04792fa24ce87ae9c848
Ahmet Özışık <ozisikahmet@gmail.com>
2019-10-14 15:23:17 +0300

    fix build

--- cypress/integration/custom-css.spec.js
+++ cypress/integration/custom-css.spec.js
@@ -1,4 +1,4 @@
-describe.only("Custom CSS injection", () => {
+describe("Custom CSS injection", () => {
   before(function() {
     indexedDB.deleteDatabase("_pouch_sessions");
   });
@@ -6,6 +6,6 @@ describe.only("Custom CSS injection", () => {
   it("injects style tag", () => {
     cy.visit(
       "/survey.html?survey=test.json&mode=online&css=body{display:none}"
-    ).find("#ssas_enketo_custom_css_stlye");
+    ).get("#ssas_enketo_custom_css_stlye");
   });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,118 +1,122 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
-const HtmlWebpackExcludeAssetsPlugin = require('html-webpack-exclude-assets-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const HtmlWebpackExcludeAssetsPlugin = require("html-webpack-exclude-assets-plugin");
 
-const inProduction = process.env.NODE_ENV === 'production'
+const inProduction = process.env.NODE_ENV === "production";
 
 const getFilename = (name, extension) => {
-    if (inProduction) {
-        return name + '.' + extension
-    }
-    return name + '.dev.' + extension
-}
+  if (inProduction) {
+    return name + "." + extension;
+  }
+  return name + ".dev." + extension;
+};
 
 const webpackConfig = {
-    entry: {
-        polyfill: 'babel-polyfill',
-        'survey': path.join(__dirname, 'src/js', 'survey'),
-        'submissions': path.join(__dirname, 'src/js', 'submissions'),
-    },
-    output: {
-        path: path.join(__dirname, 'www/build/js'),
-        filename: getFilename('[name]', 'js'),
-        libraryTarget: 'umd',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            hash: true,
-            excludeAssets: /submissions/,
-            filename: getFilename('../../survey', 'html'),
-            template: __dirname + '/src/html/survey.html',
-        }),
-        new HtmlWebpackPlugin({
-            hash: true,
-            excludeAssets: /survey/,
-            filename: getFilename('../../submissions', 'html'),
-            template: __dirname + '/src/html/submissions.html',
-        }),
-        new HtmlWebpackExcludeAssetsPlugin(),
-        new MiniCssExtractPlugin({
-            filename: getFilename('../css/[name]', 'css'),
-            chunkFilename: getFilename('../css/[name]', 'css'),
-        }),
-    ],
-    resolve: {
-        mainFields: ['browser', 'main', 'module']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    'css-loader',
-                    'sass-loader',
-                ]
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: '../../fonts/'
-                    }
-                }]
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        outputPath: '../images/'
-                    }
-                }]
+  entry: {
+    polyfill: "babel-polyfill",
+    survey: path.join(__dirname, "src/js", "survey"),
+    submissions: path.join(__dirname, "src/js", "submissions")
+  },
+  output: {
+    path: path.join(__dirname, "www/build/js"),
+    filename: getFilename("[name].[chunkhash]", "js"),
+    libraryTarget: "umd"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      excludeAssets: /submissions/,
+      filename: getFilename("../../survey", "html"),
+      template: __dirname + "/src/html/survey.html"
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      excludeAssets: /survey/,
+      filename: getFilename("../../submissions", "html"),
+      template: __dirname + "/src/html/submissions.html"
+    }),
+    new HtmlWebpackExcludeAssetsPlugin(),
+    new MiniCssExtractPlugin({
+      filename: getFilename("../css/[name]", "css"),
+      chunkFilename: getFilename("../css/[name].[chunkhash]", "css")
+    })
+  ],
+  resolve: {
+    mainFields: ["browser", "main", "module"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "../../fonts/"
             }
+          }
         ]
-    },
-    optimization: {
-        minimize: inProduction,
-        minimizer: [
-            new UglifyJsPlugin({
-                uglifyOptions: {
-                    mangle: false,
-                },
-            }),
-            new OptimizeCSSAssetsPlugin({})
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "../images/"
+            }
+          }
         ]
-    }
-}
+      }
+    ]
+  },
+  optimization: {
+    minimize: inProduction,
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          mangle: false
+        }
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  }
+};
 
-
+/*
 if (inProduction) {
-    webpackConfig.optimization.runtimeChunk = 'single'
-    webpackConfig.optimization.splitChunks = {
-       cacheGroups: {
-         vendor: {
-           test: /[\\/]node_modules[\\/]/,
-           name: 'vendors',
-           chunks: 'all'
-         }
-       }
+  webpackConfig.optimization.runtimeChunk = "single";
+  webpackConfig.optimization.splitChunks = {
+    cacheGroups: {
+      vendor: {
+        test: /[\\/]node_modules[\\/]/,
+        name: "vendors",
+        chunks: "all"
+      }
     }
-}
+  };
+}*/
 
-module.exports = webpackConfig
+module.exports = webpackConfig;
