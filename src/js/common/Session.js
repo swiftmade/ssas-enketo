823a89c2391d1a5de358fca0d5c569d9612bb813
Ahmet Özışık <ozisikahmet@gmail.com>
2019-11-21 20:52:10 +0200

    2.3 (#18)
    * clean up previous builds
    
    * session now restores last active page
    
    * fix JumpTo
    
    * build version 2.3.0

--- src/js/common/Session.js
+++ src/js/common/Session.js
@@ -1,100 +1,101 @@
-import queryParams from './QueryParams'
+import queryParams from "./QueryParams";
 
 export default class Session {
+  constructor(data) {
+    this.data = {
+      xml: null,
+      online: false,
+      draft: true,
+      payload: {},
+      _attachments: {},
+      submitted: false,
+      instance_id: null,
+      deprecated_id: null,
+      last_update: Date.now(),
+      ...data
+    };
 
-    constructor(data) {
-        this.data = {
-            xml: null,
-            online: false,
-            draft: true,
-            payload: {},
-            _attachments: {},
-            submitted: false,
-            instance_id: null,
-            deprecated_id: null,
-            last_update: Date.now(),
-            ...data,
+    if (!data.hasOwnProperty("payload")) {
+      this._setPayloadFromUrl();
+    }
+  }
+
+  setData(data) {
+    this.data = {
+      ...this.data,
+      ...data,
+      last_update: Date.now()
+    };
+    return this;
+  }
+
+  _setPayloadFromUrl() {
+    try {
+      const sessionExtra = queryParams.get("session_extra");
+      this.setData({
+        payload: JSON.parse(sessionExtra)
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  putEnketoForm(form) {
+    this.setData({
+      xml: form.xml,
+      instance_id: form.instance_id,
+      deprecated_id: form.deprecated_id,
+      current_page: form.current_page,
+      _attachments: this._getFiles(form.files)
+    });
+  }
+
+  toFormInstance() {
+    return {
+      instanceStr: this.data.xml,
+      submitted: this.data.submitted
+    };
+  }
+
+  isOnline() {
+    return this.data.online;
+  }
+
+  _getFiles(files) {
+    if (!files || typeof files !== "object") {
+      return this.data._attachments;
+    }
+
+    const attachments = {};
+
+    files.forEach(file => {
+      // Mark already existing attachments as stub
+      if (this._isFileStub(file)) {
+        if (this._attachmentExists(file)) {
+          attachments[file] = {
+            ...this.data._attachments[file],
+            stub: true
+          };
         }
+        return;
+      }
+      attachments[file.name] = {
+        data: file,
+        content_type: file.type
+      };
+    });
 
-        if (!data.hasOwnProperty('payload')) {
-            this._setPayloadFromUrl()
-        }
-    }
+    return attachments;
+  }
 
-    setData(data) {
-        this.data = {
-            ...this.data,
-            ...data,
-            last_update: Date.now(),
-        }
-        return this
-    }
+  _isFileStub(file) {
+    return typeof file === "string";
+  }
 
-    _setPayloadFromUrl() {
-        try {
-            const sessionExtra = queryParams.get('session_extra')
-            this.setData({
-                payload: JSON.parse(sessionExtra)
-            })
-        } catch(e) {
-            console.error(e)
-        }
-        
-    }
-
-    putEnketoForm(form) {
-        this.setData({
-            xml: form.xml,
-            instance_id: form.instance_id,
-            deprecated_id: form.deprecated_id,
-            _attachments: this._getFiles(form.files)
-        })
-    }
-
-    toFormInstance() {
-        return {
-            instanceStr: this.data.xml,
-            submitted: this.data.submitted,
-        }
-    }
-
-    isOnline() {
-        return this.data.online
-    }
-
-    _getFiles(files) {
-        if (!files || typeof files !== 'object') {
-            return this.data._attachments
-        }
-
-        const attachments = {}
-
-        files.forEach(file => {
-            // Mark already existing attachments as stub
-            if (this._isFileStub(file)) {
-                if (this._attachmentExists(file)) {
-                    attachments[file] = {
-                        ...this.data._attachments[file],
-                        stub: true
-                    }
-                }
-                return
-            }
-            attachments[file.name] = {
-                data: file,
-                content_type: file.type,
-            }
-        })
-
-        return attachments
-    }
-
-    _isFileStub(file) {
-        return typeof file === 'string'
-    }
-
-    _attachmentExists(name) {
-        return this.data.hasOwnProperty('_attachments')
-            && this.data._attachments.hasOwnProperty(name)
-    }
+  _attachmentExists(name) {
+    return (
+      this.data.hasOwnProperty("_attachments") &&
+      this.data._attachments.hasOwnProperty(name)
+    );
+  }
 }
--- src/js/survey/ui/JumpTo.js
+++ src/js/survey/ui/JumpTo.js
@@ -1,92 +1,95 @@
-import angular from 'angular'
-import vAccordion from 'v-accordion'
-const emitter = require('tiny-emitter/instance')
-import queryParams from '../../common/QueryParams'
+import angular from "angular";
+import vAccordion from "v-accordion";
+const emitter = require("tiny-emitter/instance");
+import queryParams from "../../common/QueryParams";
+import EnketoForm from "../../survey/EnketoForm";
 
 const setupJumpTo = () => {
+  $("#jump-to-block").hide();
 
-    $("#jump-to-block").hide();
+  // Jump To module can be disabled
+  if (queryParams.get("jumpto") === "off") {
+    $("#jump-to").remove();
+    return;
+  }
 
-    // Jump To module can be disabled
-    if (queryParams.get('jumpto') === 'off') {
-        $("#jump-to").remove()
-        return
-    }
+  var app = angular.module("jumpTo", ["vAccordion"]);
+  var _ = require("../../common/helpers");
 
-    var app = angular.module('jumpTo', ['vAccordion'])
-    var _ = require('../../common/helpers')
+  app.controller("jumpCtrl", [
+    "$scope",
+    function($scope) {
+      var rawPages = [];
+      $scope.pages = {};
+      $scope.search = {
+        label: ""
+      };
 
-    app.controller("jumpCtrl", ['$scope', function ($scope) {
-        var rawPages = [];
-        $scope.pages = {};
-        $scope.search = {
-            label: ""
-        };
+      emitter.on("EnketoForm.initialized", () =>
+        $scope.$apply(() => {
+          $('[role="page"]').each(function() {
+            var page = $(this);
+            var labels = $(".question-label.active", page);
 
-        emitter.on('EnketoForm.initialized', () => $scope.$apply(() => {
-        
-            $('[role="page"]').each(function () {
-                var page = $(this);
-                var labels = $(".question-label.active", page);
+            labels.each(function() {
+              var label = $(this).text();
 
-                labels.each(function () {
-                    var label = $(this).text();
+              if (label.length < 2 || label[1] != ".") {
+                return;
+              }
 
-                    if (label.length < 2 || label[1] != ".") {
-                        return;
-                    }
-
-                    var firstSpace = label.indexOf(" ");
-                    var notations = label.substr(0, firstSpace)
-                        .split(".")
-                        .filter(function (str) {
-                            return str != '';
-                        });
-
-                    rawPages.push({
-                        el: page.children().first(),
-                        notations: notations,
-                        label: label
-                    });
+              var firstSpace = label.indexOf(" ");
+              var notations = label
+                .substr(0, firstSpace)
+                .split(".")
+                .filter(function(str) {
+                  return str != "";
                 });
+
+              rawPages.push({
+                el: page.children().first(),
+                notations: notations,
+                label: label
+              });
             });
+          });
 
-            rawPages.forEach(function (page) {
-                _.set($scope.pages, page.notations.join(".items."), page);
-                if (page.notations.length > 1) {
-                    // lose something in the beginning
-                    page.notations.pop();
-                    var parentPage = page.notations.join(".items.");
+          rawPages.forEach(function(page) {
+            _.set($scope.pages, page.notations.join(".items."), page);
+            if (page.notations.length > 1) {
+              // lose something in the beginning
+              page.notations.pop();
+              var parentPage = page.notations.join(".items.");
 
-                    if (_.get($scope.pages, parentPage + ".label", null) == null) {
-                        _.set($scope.pages, parentPage + ".label", page.label);
-                        _.set($scope.pages, parentPage + ".el", page.el);
-                    }
-                }
-            });
+              if (_.get($scope.pages, parentPage + ".label", null) == null) {
+                _.set($scope.pages, parentPage + ".label", page.label);
+                _.set($scope.pages, parentPage + ".el", page.el);
+              }
+            }
+          });
+        })
+      );
 
-        }))
-
-        $scope.jump = function (page) {
-            $scope.accordion.collapseAll();
-            form.pages.flipToPageContaining(page.el);
-            setTimeout(function () {
-                $("#jump-to-block").overlay("hide");
-            });
-        };
-
-        $("#jump-to-close").click(function () {
-            $scope.accordion.collapseAll();
-            $("#jump-to-block").overlay("hide");
+      $scope.jump = function(page) {
+        $scope.accordion.collapseAll();
+        EnketoForm.form.pages.flipToPageContaining(page.el);
+        setTimeout(function() {
+          $("#jump-to-block").overlay("hide");
         });
-    }]);
+      };
 
-    $("#jump-to").click(function () {
-        $("#jump-to-block").overlay("show");
-    });
+      $("#jump-to-close").click(function() {
+        $scope.accordion.collapseAll();
+        $("#jump-to-block").overlay("hide");
+      });
+    }
+  ]);
 
-    angular.bootstrap(document.getElementById('jump-to-block'), ["jumpTo"])
-}
+  $("#jump-to").click(function() {
+    $("#jump-to-block").overlay("show");
+  });
 
-setupJumpTo()
+  angular.bootstrap(document.getElementById("jump-to-block"), ["jumpTo"]);
+};
 
+setupJumpTo();
